b07fb2db92c809ef2f28bb5e6edf28349e1e1395
Rizhi-KotE <d1mqn1ch@mail.ru>
2016-09-26 18:06:40 +0300

    батле

---
+++ src/main/webapp/battleComponent/BattleModule.js
@@ -0,0 +1,28 @@
+(function () {
+    angular.module("battleModule", ["ui.router"])
+        .config(["$stateProvider", function battleStateConfig($stateProvider) {
+            $stateProvider
+                .state("battle", {
+                    url: "",
+                    views: {
+                        "": {
+                            templateUrl: "battleComponent/battleTemplate.html",
+                            controller: "battleController",
+                            controllerAs: "vm"
+                        }
+                    }
+                })
+        }])
+        .controller("battleController", ["$scope", function battleController($scope) {
+            var vm = this;
+            $scope.lines = [];
+            for (var i = 0; i < 10; i++) {
+                var line = [];
+                for (var j = 0; j < 10; j++) {
+                    var cell = {};
+                    line.push(cell);
+                }
+                $scope.lines.push(line);
+            }
+        }])
+})()
